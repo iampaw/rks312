aa66efb2d3b1bda2e1686c4c233b1d5286844f4e
Zulfajri <zulfajrisurya30@gmail.com>
2024-10-07 06:52:49 +0000

    update program

--- webku/urls.py
+++ webku/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login_view, name='login'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout_view, name='logout'),
 ]
--- webku/views.py
+++ webku/views.py
@@ -1,3 +1,4 @@
+# webku/views.py
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.contrib.auth.forms import UserCreationForm
@@ -6,29 +7,31 @@ from django.contrib import messages
 def home(request):
     return render(request, 'home.html')
 
-def login_view(request):
-    if request.method == 'POST':
+def signup(request):
+    # Jika request POST untuk login
+    if 'login' in request.POST:
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             auth_login(request, user)
-            return redirect('home')  # Redirect to home after successful login
+            return redirect('home')
         else:
             messages.error(request, 'Invalid username or password')
-    return render(request, 'signup.html')
 
-def signup(request):
-    if request.method == 'POST':
+    # Jika request POST untuk signup
+    elif 'signup' in request.POST:
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Account created successfully!')
-            return redirect('login')
+            return redirect('signup')
+
     else:
         form = UserCreationForm()
+
     return render(request, 'signup.html', {'form': form})
 
 def logout_view(request):
     auth_logout(request)
-    return redirect('home')  # Redirect to home after logout
+    return redirect('home')
