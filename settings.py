1addd31ad5f88ad709c76e6df0e76858b2ae1982
iampaw <ip387257@gmail.com>
2024-10-18 15:42:26 +0000

    modifikasi halaman utama login,sign up

--- settings.py
+++ settings.py
@@ -125,10 +125,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/webku/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'webku/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
--- webku/urls.py
+++ webku/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home_page, name='home'),
@@ -8,3 +10,6 @@ urlpatterns = [
     path('login/', views.custom_login, name='login'),
     path('logout/', views.logout_view, name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
